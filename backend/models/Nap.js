1b0f614b6aa37727c8d8320b5529c3ff1e4ef762
Luc Nguyen <lucnguyen1416@gmail.com>
2025-04-22 18:10:37 -0700

    found new template to use

--- backend/models/Nap.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const napSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  time: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Nap", napSchema);
