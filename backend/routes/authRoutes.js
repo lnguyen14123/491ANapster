f8e49e35443780f9bc29f5c65a8d03806cde9ab7
Luc Nguyen <lucnguyen1416@gmail.com>
2025-09-02 13:09:01 -0700

    minor changes new semester

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,6 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import bcrypt from "bcrypt";
 import { pool } from "../db.js";
+
 // __dirname workaround for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
